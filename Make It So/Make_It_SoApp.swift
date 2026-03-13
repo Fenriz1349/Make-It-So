f31e62af14c50c35050851cb3344575dadd1f7a1
Julien Cotte <129265577+Fenriz1349@users.noreply.github.com>
2026-03-13 08:50:28 +0100

    Initial Commit

---
+++ Make It So/Make_It_SoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_It_SoApp.swift
+//  Make It So
+//
+//  Created by Julien Cotte on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Make_It_SoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
